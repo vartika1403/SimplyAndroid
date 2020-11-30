f2a183e0d6f694aaae7ed4da5ffe07dc1adca423
vartika1403 <vartika.1403@gmail.com>
2020-12-01 00:43:18 +0530

    added recycler view and crountines to handle the response

--- app/src/main/java/com/example/appnetworking/MainActivity.kt
+++ app/src/main/java/com/example/appnetworking/MainActivity.kt
@@ -3,17 +3,20 @@ package com.example.appnetworking
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main2.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.flow.collect
 
 @ExperimentalCoroutinesApi
 class MainActivity : AppCompatActivity() {
     private lateinit var counter:String
+    private lateinit var adapter: MainAdapter
     companion object X {
         val TAG = "MainActivity"
     }
@@ -27,12 +30,51 @@ class MainActivity : AppCompatActivity() {
 
         initCountObsever()
         initView()
+        setUpObservers();
+    }
+
+    private fun setUpObservers(){
+        viewModel.getUsers().observe(this, Observer {
+           it?.let {
+               resource ->
+               when (resource.status) {
+                   Status.SUCCESS -> {
+                       recyclerView.visibility = View.VISIBLE
+                       progressBar.visibility = View.GONE
+                       resource.data?.let { users -> retrieveList(users) }
+                   }
+                   Status.ERROR -> {
+                       recyclerView.visibility = View.VISIBLE
+                       progressBar.visibility = View.GONE
+                       Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+                   }
+                   Status.LOADING -> {
+                       progressBar.visibility = View.VISIBLE
+                       recyclerView.visibility = View.GONE
+                   }
+               }
+           }
+        })
+    }
+
+    private fun retrieveList(users: List<User>) {
+        adapter.apply {
+            addUsers(users)
+            notifyDataSetChanged()
+        }
     }
 
     private fun initView(){
         counter_plus.setOnClickListener(::incrementCounter)
         counter_minus.setOnClickListener(::decrementCounter)
 
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MainAdapter((arrayListOf()))
+        recyclerView.addItemDecoration(DividerItemDecoration(
+                recyclerView.context,
+                (recyclerView.layoutManager as LinearLayoutManager).orientation
+        ))
+        recyclerView.adapter = adapter
     }
 
     private fun incrementCounter(view: View) {
---
+++ app/src/main/java/com/example/appnetworking/MainAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.appnetworking
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.row_item.view.*
+
+ class MainAdapter(private val usersList: ArrayList<User>): RecyclerView.Adapter<MainAdapter.DataViewHolder>() {
+
+    class DataViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(user: User) {
+            itemView.apply {
+                textViewUserName.text = user.name
+                textViewUserEmail.text = user.email
+
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataViewHolder =
+        DataViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.row_item, parent, false))
+
+
+    override fun onBindViewHolder(holder: DataViewHolder, position: Int) {
+        Log.d("vartika", "userList[pos]" + usersList[position].name + " pos: " + position)
+       holder.bind(usersList[position])
+    }
+
+    override fun getItemCount(): Int = usersList.size
+
+     fun addUsers(users: List<User>) {
+         this.usersList.apply {
+             clear()
+             addAll(users)
+         }
+
+     }
+}
